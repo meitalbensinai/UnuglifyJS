c69f5f1b77f6261513f33bc501f82630b6034cce
Uri Alon <urialon1@gmail.com>
2017-01-06 20:43:29 +0200

    randomPigeonExp.sh

--- all_results.sh
+++ all_results.sh
@@ -1,3 +1,4 @@
-python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 4,5,6,7 --num_threads 32 --path_width 1
+#python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 4,5,6,7 --num_threads 32 --path_width 1
+python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 8 --num_threads 32 --path_width 1
 python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 4,5,6,7,8 --num_threads 32 --path_width 2
-python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 4,5,6,7 --num_threads 32 --path_width 3
+python path_length_test.py --training_dir /homes/urialon/js_training/ --test_dir /homes/urialon/js_test/ --nice2predict_server localhost:5745 --path_lengths 4,5,6,7,8 --num_threads 32 --path_width 3
---
+++ randomPigeonExp.sh
@@ -0,0 +1,4 @@
+for fraction in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+do
+    python randomPigeon.py training_data_7x1 > training_data_7x1_$fraction
+done
