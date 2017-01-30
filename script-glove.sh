443a94d6cfc488f65a44055456745750ba279e93
Uri Alon <urialon1@gmail.com>
2017-01-30 09:04:37 +0200

    script-glove

--- script-glove.sh
+++ script-glove.sh
@@ -1,3 +1,11 @@
-python extract_features_to_glove.py --dir /homes/urialon/jsnice_training_clean/ --max_path_length 6 --max_path_width 2 > glove_models/jsnice_training_clean6x2 2> paths-nohup.out
+if [ $# -lt 2 ]
+    then
+        echo "Usage: script-glove.sh <length> <width>"
+        exit 1
+fi
+length=$1
+width=$2
+
+python extract_features_to_glove.py --dir /homes/urialon/jsnice_training_clean/ --max_path_length ${length} --max_path_width ${width} > glove_models/jsnice_training_clean${length}x${width}.txt 2> paths-nohup.out
 cd ../glove/
-./uridemo3.sh
+./uridemo.sh ${length} ${width}
