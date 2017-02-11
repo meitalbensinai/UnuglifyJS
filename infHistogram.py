209f4ee93d86dc4e41412b930f88bddae1ff7b7e
Uri Alon <urialon1@gmail.com>
2017-02-11 20:28:39 +0200

    infHisto - descending + catch json exception

--- infHistogram.py
+++ infHistogram.py
@@ -13,32 +13,35 @@ def strip(word):
 
 
 if __name__ == '__main__':
-	trainingFileName = sys.argv[1]
-	histogram = {}
-	include_giv = False
-	if (len(sys.argv) > 2 and sys.argv[2] == '--include_giv'):
-		include_giv = True
+    trainingFileName = sys.argv[1]
+    histogram = {}
+    include_giv = False
+    if (len(sys.argv) > 2 and sys.argv[2] == '--include_giv'):
+        include_giv = True
 
-	with open(trainingFileName, "r") as trainingFile:
-		for line in trainingFile:
-			line = line.rstrip('\n')
-			singleProgramObject = json.loads(line)
-			assign = singleProgramObject['assign']
-			for varItem in assign:
-				if (varItem.has_key('inf')):
-					name = strip(varItem['inf'])
-					if histogram.has_key(name):
-						histogram[name] += 1
-					else:
-						histogram[name] = 1
-				elif (varItem.has_key('giv') and include_giv):
-					name = strip(varItem['giv'])
-					if histogram.has_key(name):
-						histogram[name] += 1
-					else:
-						histogram[name] = 1
+    with open(trainingFileName, "r") as trainingFile:
+        for line in trainingFile:
+            line = line.rstrip('\n')
+            try:
+                singleProgramObject = json.loads(line)
+            except ValueError:
+                continue
+            assign = singleProgramObject['assign']
+            for varItem in assign:
+                if (varItem.has_key('inf')):
+                    name = strip(varItem['inf'])
+                    if histogram.has_key(name):
+                        histogram[name] += 1
+                    else:
+                        histogram[name] = 1
+                elif (varItem.has_key('giv') and include_giv):
+                    name = strip(varItem['giv'])
+                    if histogram.has_key(name):
+                        histogram[name] += 1
+                    else:
+                        histogram[name] = 1
                        
-	top = dict(Counter(histogram))
-	sortedTopValues = OrderedDict(sorted(top.items(), key=lambda t: t[1]))
-	for key,value in sortedTopValues.items():
-		print str(key) + "," + str(value)
+    top = dict(Counter(histogram))
+    sortedTopValues = OrderedDict(sorted(top.items(), key=lambda t: t[1], reverse=True))
+    for key,value in sortedTopValues.items():
+        print str(key) + "," + str(value)
